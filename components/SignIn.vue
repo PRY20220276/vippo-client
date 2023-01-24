3dce1d5e911990e293520465157126ff31eacf9d
ee8894 <joseluismorales.watanabe@externos-cl.cencosud.com>
2023-01-24 16:28:29 -0500

    add sign in component

--- components/SignIn.vue
+++ components/SignIn.vue
@@ -4,45 +4,32 @@
             <template v-slot:activator="{ props }">
                 <v-btn variant="outlined" class="ma-2" color="white" v-bind="props">Sign In</v-btn>
             </template>
-            <v-card class="text-center">
-                <div class="text-right">
-                    <v-btn icon="mdi-close" color="gray" variant="text" @click="dialog = false"
-                        style="margin: 8px;"></v-btn>
-                </div>
-                <v-card-title class="text-h5 font-weight-bold">
-                    Iniciar Sesión
-                </v-card-title>
-
-                <v-card-text>
-                    <p class="text-medium-emphasis my-2">
-                        Potencia tus proyectos de video
-                        con Inteligencia Artifical 🤖
-
-                    </p>
-                    <v-btn color="primary" class="my-2" rounded="lg" @click="signIn()">Iniciar sesión con
-                        email</v-btn>
-
-                </v-card-text>
-                <v-card-actions style="display:block">
-                    <p class="text-caption mt-5">By using Vippo, you agree to our<br /><a href="#">Privacy
-                            Policy</a>
-                        and <a href="#">Terms of Use</a>.</p>
-
-                </v-card-actions>
-            </v-card>
+            <component :is="steps[currentStepIndex]" @submit="onClickTab()" @closeDialog="close()"></component>
         </v-dialog>
+
     </div>
 
 </template>
 <script>
-
+import SignInStepOne from "./SignIn/SignInStepOne.vue"
+import SignInStepTwo from "./SignIn/SignInStepTwo.vue"
 export default {
+    components: {
+        SignInStepOne,
+        SignInStepTwo
+    },
     data: () => ({
-        dialog: false
+        dialog: false,
+        steps: ["SignInStepOne", "SignInStepTwo"],
+        currentStepIndex: 0
     }),
     methods: {
-        signIn() {
-            alert("Signin Passwordless")
+        onClickTab() {
+            //alert("Signin Passwordless")
+            this.currentStepIndex++
+        },
+        close() {
+            //this.dialog = false
         }
     }
 }
---
+++ components/SignIn/SignInStepTwo.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card class="text-center" :ripple="false">
+        <div class="text-right">
+            <v-btn icon="mdi-close" color="gray" variant="text" @click="onCloseDialog()"></v-btn>
+        </div>
+        <div class="text-h5 font-weight-bold">
+            Iniciar Sesión
+        </div>
+        <p class="text-medium-emphasis">
+            Enviaremos un código a tu correo
+
+        </p>
+        <v-card-text>
+            <v-text-field label="Email"></v-text-field>
+            <v-btn color="primary" block class="my-2" rounded="lg" variant="flat"
+                @click.stop="onClick()">Continuar</v-btn>
+        </v-card-text>
+    </v-card>
+</template>
+<script lang="js">
+export default {
+    data: () => ({
+
+    }),
+    methods: {
+        onClick() {
+            this.$emit("click", true)
+        },
+        onCloseDialog() {
+            this.$emit("closeDialog")
+        }
+    }
+}
+</script>

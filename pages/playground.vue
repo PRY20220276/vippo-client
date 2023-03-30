54356cba1cd0029d1af07b103dc58456fb23ffc9
MarcoMnrq <63822742+MarcoMnrq@users.noreply.github.com>
2023-03-29 23:37:55 -0500

    feat: add playground to test new components

---
+++ pages/playground.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <v-breadcrumbs :items="['Foo', 'Bar', 'Fizz']"></v-breadcrumbs>
+    <div class="ml-4 text-h4 text-primary">Playground</div>
+    <v-row class="mt-5">
+      <v-col v-for="n in 6" :key="n" cols="12" sm="4">
+        <!-- Test out components here -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "PlaygroundPage",
+  data: () => ({}),
+};
+</script>

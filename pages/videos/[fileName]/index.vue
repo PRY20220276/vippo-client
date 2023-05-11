297bdbc13a51aae5b698417661352c2a13532c19
Piton077 <93939928+Piton077@users.noreply.github.com>
2023-05-11 15:35:06 -0500

    feat: show custom summaries (#53)
    Co-authored-by: ee8894 <joseluismorales.watanabe@externos-cl.cencosud.com>

--- pages/videos/[fileName]/index.vue
+++ pages/videos/[fileName]/index.vue
@@ -2,11 +2,8 @@
   <v-container fluid>
     <!-- Page Breadcrumbs -->
 
-    <v-breadcrumbs
-      :items="['VIPPO', 'Videos', $route.params.fileName]"
-      bg-color="#293040ff"
-      class="text-body-2 text-white"
-    ></v-breadcrumbs>
+    <v-breadcrumbs :items="['VIPPO', 'Videos', $route.params.fileName]" bg-color="#293040ff"
+      class="text-body-2 text-white"></v-breadcrumbs>
     <!-- End: Page Breadcrumbs -->
     <!-- Page Toolbar -->
     <v-toolbar color="background" class="text-primary mt-4">
@@ -22,12 +19,7 @@
     <v-row v-if="video">
       <v-col>
         <v-card color="black">
-          <video
-            ref="videoElement"
-            :src="video.downloadPath"
-            controls
-            class="fit-video"
-          ></video>
+          <video ref="videoElement" :src="video.downloadPath" controls class="fit-video"></video>
         </v-card>
       </v-col>
     </v-row>
@@ -37,12 +29,7 @@
         Video Summarization
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        color="secondary"
-        @click="hideSummary = !hideSummary"
-        size="small"
-      >
+      <v-btn icon color="secondary" @click="hideSummary = !hideSummary" size="small">
         <v-icon>{{ hideSummary ? "mdi-eye-off-outline" : "mdi-eye" }}</v-icon>
       </v-btn>
     </v-toolbar>
@@ -61,43 +48,23 @@
               <v-window-item value="summary_transcript">
                 <v-row v-if="video.meta.podcast_summarization">
                   <v-col>
-                    <video
-                      controls
-                      style="object-fit: contain; width: 100%; height: 100%"
-                      :src="video.meta.podcast_summarization[0]"
-                    ></video>
+                    <video controls style="object-fit: contain; width: 100%; height: 100%"
+                      :src="video.meta.podcast_summarization"></video>
                   </v-col>
                 </v-row>
-                <v-row
-                  v-if="video.meta.podcast_summarization"
-                  justify="center"
-                  align="center"
-                  class="mb-3"
-                >
+                <v-row v-if="video.meta.podcast_summarization" justify="center" align="center" class="mb-3">
                   <v-col cols="auto">
-                    <v-btn
-                      color="primary"
-                      prepend-icon="mdi-download"
-                      density="comfortable"
-                    >
+                    <v-btn color="primary" prepend-icon="mdi-download" density="comfortable">
                       Download video
                     </v-btn>
                   </v-col>
                   <v-col cols="auto">
-                    <v-btn
-                      color="#1877F2"
-                      prepend-icon="mdi-facebook"
-                      density="comfortable"
-                    >
+                    <v-btn color="#1877F2" prepend-icon="mdi-facebook" density="comfortable">
                       Share on Facebook
                     </v-btn>
                   </v-col>
                   <v-col cols="auto">
-                    <v-btn
-                      color="#E1306C"
-                      prepend-icon="mdi-instagram"
-                      density="comfortable"
-                    >
+                    <v-btn color="#E1306C" prepend-icon="mdi-instagram" density="comfortable">
                       Share on Instagram
                     </v-btn>
                   </v-col>
@@ -113,13 +80,39 @@
                 {{ video.meta.objectSummary || [] }}
               </v-window-item>
               <v-window-item value="custom_summary">
-                <v-row justify="center" align="center" class="my-5">
-                  <v-col cols="auto">
-                    <v-btn
-                      prepend-icon="mdi-auto-fix"
-                      @click="() => moveToNewSummarizationPage()"
-                      >Create custom summary</v-btn
-                    >
+                <div class="my-2" style="text-align: right;">
+                  <v-btn prepend-icon="mdi-auto-fix" @click="() => moveToNewSummarizationPage()">Create custom
+                    summary</v-btn>
+                </div>
+                <v-row v-if="video.meta.custom_summaries">
+                  <v-col cols="3">
+                    <v-card>
+                      <v-card-text>
+                        <v-text-field v-model="chapterFilter" density="compact" variant="solo" label="Search chapters"
+                          append-inner-icon="mdi-magnify" single-line hide-details></v-text-field>
+                        <h3 class="mt-4 text-center" v-if="!filteredCustomSummaries.length">No chapters found</h3>
+                      </v-card-text>
+                      <template v-if="filteredCustomSummaries.length">
+                        <v-list>
+                          <v-list-item :disabled="item.state == 'pending'"
+                            :active="video.meta.custom_summaries[selectedCustomSummaryIndex] == item.chapter"
+                            v-for="(item, i) in  filteredCustomSummaries " :key="i"
+                            @click="() => handleSelectChapter(item.chapter)">
+                            <template v-slot:append>
+                              <v-icon icon="mdi-chevron-right"></v-icon>
+                            </template>
+
+                            <v-list-item-title v-text="item.chapter"></v-list-item-title>
+                          </v-list-item>
+                        </v-list>
+                      </template>
+
+
+                    </v-card>
+                  </v-col>
+                  <v-col>
+                    <video controls style="object-fit: contain; width: 100%; height: 100%"
+                      :src="video.meta.custom_summaries[selectedCustomSummaryIndex].url"></video>
                   </v-col>
                 </v-row>
               </v-window-item>
@@ -130,12 +123,9 @@
     </v-row>
     <v-row v-else class="mt-4">
       <v-col>
-        <v-alert
-          type="info"
-          title="Analysis Not Found"
+        <v-alert type="info" title="Analysis Not Found"
           text="This means that your video is still being processed in the backend. Please return later to see the results."
-          variant="tonal"
-        ></v-alert>
+          variant="tonal"></v-alert>
       </v-col>
     </v-row>
     <!-- End: Video Player Row -->
@@ -145,12 +135,7 @@
         Video Analysis
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        color="secondary"
-        @click="hideAnalysis = !hideAnalysis"
-        size="small"
-      >
+      <v-btn icon color="secondary" @click="hideAnalysis = !hideAnalysis" size="small">
         <v-icon>{{ hideAnalysis ? "mdi-eye-off-outline" : "mdi-eye" }}</v-icon>
       </v-btn>
     </v-toolbar>
@@ -171,29 +156,18 @@
             <v-window v-model="tab">
               <v-window-item value="labels">
                 <v-expansion-panels>
-                  <v-expansion-panel
-                    v-for="(obj, index) in video.meta.labels"
-                    :key="index"
-                  >
-                    <v-expansion-panel-title
-                      class="text-capitalize font-weight-bold"
-                    >
+                  <v-expansion-panel v-for="( obj, index ) in  video.meta.labels " :key="index">
+                    <v-expansion-panel-title class="text-capitalize font-weight-bold">
                       {{ obj.label }}
                       <span class="ml-2 font-weight-light">
-                        {{ obj.segments.length }} segments</span
-                      >
+                        {{ obj.segments.length }} segments</span>
                     </v-expansion-panel-title>
                     <v-expansion-panel-text>
                       <v-chip-group>
-                        <v-chip
-                          v-for="(timestamp, index) in obj.segments"
-                          :key="index"
-                          @click="
-                            $refs.videoElement.currentTime = timestamp.startTime
-                          "
-                          >{{ secondsToTimestamp(timestamp.startTime) }} -
-                          {{ secondsToTimestamp(timestamp.endTime) }}</v-chip
-                        >
+                        <v-chip v-for="( timestamp, index ) in  obj.segments " :key="index" @click="
+                          $refs.videoElement.currentTime = timestamp.startTime
+                          ">{{ secondsToTimestamp(timestamp.startTime) }} -
+                          {{ secondsToTimestamp(timestamp.endTime) }}</v-chip>
                       </v-chip-group>
                     </v-expansion-panel-text>
                   </v-expansion-panel>
@@ -202,29 +176,18 @@
 
               <v-window-item value="objects">
                 <v-expansion-panels>
-                  <v-expansion-panel
-                    v-for="(obj, index) in video.meta.objects"
-                    :key="index"
-                  >
-                    <v-expansion-panel-title
-                      class="text-capitalize font-weight-bold"
-                    >
+                  <v-expansion-panel v-for="( obj, index ) in  video.meta.objects " :key="index">
+                    <v-expansion-panel-title class="text-capitalize font-weight-bold">
                       {{ obj.name }}
                       <span class="ml-2 font-weight-light">
-                        {{ obj.timestamps.length }} segments</span
-                      >
+                        {{ obj.timestamps.length }} segments</span>
                     </v-expansion-panel-title>
                     <v-expansion-panel-text>
                       <v-chip-group>
-                        <v-chip
-                          v-for="(timestamp, index) in obj.timestamps"
-                          :key="index"
-                          @click="
-                            $refs.videoElement.currentTime = timestamp.startTime
-                          "
-                          >{{ secondsToTimestamp(timestamp.startTime) }} -
-                          {{ secondsToTimestamp(timestamp.endTime) }}</v-chip
-                        >
+                        <v-chip v-for="( timestamp, index ) in  obj.timestamps " :key="index" @click="
+                          $refs.videoElement.currentTime = timestamp.startTime
+                          ">{{ secondsToTimestamp(timestamp.startTime) }} -
+                          {{ secondsToTimestamp(timestamp.endTime) }}</v-chip>
                       </v-chip-group>
                     </v-expansion-panel-text>
                   </v-expansion-panel>
@@ -234,13 +197,9 @@
               <v-window-item value="explicit">
                 <v-row>
                   <v-col>
-                    <v-chip
-                      v-for="(obj, index) in video.meta.explicitContent"
-                      :key="index"
-                      @click="$refs.videoElement.currentTime = obj.offsetTime"
-                      :color="getChipColor(obj.explicit_tag)"
-                      class="mr-1 mb-1"
-                    >
+                    <v-chip v-for="( obj, index ) in  video.meta.explicitContent " :key="index"
+                      @click="$refs.videoElement.currentTime = obj.offsetTime" :color="getChipColor(obj.explicit_tag)"
+                      class="mr-1 mb-1">
                       {{ secondsToTimestamp(obj.offsetTime) }}
                     </v-chip>
                   </v-col>
@@ -249,9 +208,7 @@
 
               <v-window-item value="debug">
                 <pre
-                  style="background-color: black; color: white; padding: 1rem"
-                  >{{ JSON.stringify(video, null, 2) }}</pre
-                >
+                  style="background-color: black; color: white; padding: 1rem">{{ JSON.stringify(video, null, 2) }}</pre>
               </v-window-item>
             </v-window>
           </v-card-text>
@@ -260,12 +217,9 @@
     </v-row>
     <v-row v-else class="mt-4">
       <v-col>
-        <v-alert
-          type="info"
-          title="Analysis Not Found"
+        <v-alert type="info" title="Analysis Not Found"
           text="This means that your video is still being processed in the backend. Please return later to see the results."
-          variant="tonal"
-        ></v-alert>
+          variant="tonal"></v-alert>
       </v-col>
     </v-row>
 
@@ -278,6 +232,7 @@ definePageMeta({
 });
 </script>
 <script>
+
 export default {
   name: "VideoDetailsPage",
   data: () => ({
@@ -286,13 +241,25 @@ export default {
     tabSummary: "summary_transcript",
     hightlight: 0,
     hideSummary: false,
+    chapterFilter: "",
+    selectedCustomSummaryIndex: 0,
+    customSummaries: [{ "chapter": "a New begginning", "url": "https://storage.googleapis.com/download/storage/v1/b/vippo-bucket-media-dev/o/1%2FProgrammer-05092023035200PM.mp4?generation=1683647523965599&alt=media", "state": "done" }, { "chapter": "a New begginning", "url": "https://storage.googleapis.com/download/storage/v1/b/vippo-bucket-media-dev/o/1%2FArthur_Joker-05012023115313PM.mp4?generation=1682985203412565&alt=media", "state": "pending" }],
     hideAnalysis: false,
   }),
   mounted() {
     this.fetchVideo();
   },
-  computed: {},
+  computed: {
+    filteredCustomSummaries() {
+      if (!this.video) return []
+      if (!this.chapterFilter) return this.video.meta.custom_summaries;
+      return this.video.meta.custom_summaries.filter((s) => s.chapter.toLocaleLowerCase().includes(this.chapterFilter.toLocaleLowerCase()))
+    }
+  },
   methods: {
+    handleSelectChapter(chapter) {
+      this.selectedCustomSummaryIndex = this.video.meta.custom_summaries.findIndex(x => x.chapter == chapter)
+    },
     getChipColor(likeliness) {
       switch (likeliness) {
         case "VERY-LIKELY":
